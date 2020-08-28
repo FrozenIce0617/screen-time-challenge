0ea2b92d2397e6b067123d26408a46ea23b91e24
FrozenIce0617 <prosdev087@gmail.com>
2020-08-28 14:38:25 +0100

    feat: add theme constants for both light and dark theme

---
+++ src/theme/dark.js
@@ -0,0 +1,22 @@
+const darkTheme = {
+  text: {
+    primary: '#e2e8ee',
+    secondary: '#9399a5',
+    buttonActive: '#fff',
+    buttonInactive: '#1da0f2',
+  },
+  background: {
+    primary: '#253341',
+    secondary: '#151f2b',
+    buttonActive: '#1da0f2',
+    buttonInactive: '#fff',
+  },
+  header: {
+    primary: '#fff',
+  },
+  border: {
+    primary: '#6a665e',
+  },
+};
+
+export default darkTheme;
---
+++ src/theme/light.js
@@ -0,0 +1,22 @@
+const lightTheme = {
+  text: {
+    primary: '#282e33',
+    secondary: '#9399a5',
+    buttonActive: '#fff',
+    buttonInactive: '#1da0f2',
+  },
+  background: {
+    primary: '#fff',
+    secondary: '#252f3b',
+    buttonActive: '#1da0f2',
+    buttonInactive: '#fff',
+  },
+  header: {
+    primary: '#fff',
+  },
+  border: {
+    primary: '#6a665e',
+  },
+};
+
+export default lightTheme;
